c9d77ea5b87d6e00b7d4f626dc86f4de30492ece
Hasan Hüseyin <hasanhuseyinbagg@gmail.com>
2021-09-18 10:21:55 +0300

    Fırlatma komutu yazıldı
    - Belirli aralıklarla (gerçek oyunda olduğu gibi 7 atışta bir) dev fırlatılıyor.

---
+++ MobControl/Assets/Scripts/Fire.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    [SerializeField] GameObject Insan, Dev;
+    [SerializeField] Transform RayPoint;
+    CannonScript cannon = new CannonScript();
+    int AtilanInsanSayisi = 0;
+    float FireRateTimer;
+    void Start()
+    {
+        cannon.FireRate = 0.5f;
+    }
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && Time.time > FireRateTimer)
+        {
+            Shot();
+            AtilanInsanSayisi++;
+            FireRateTimer = Time.time + cannon.FireRate;
+        }
+    }
+    public void Shot()
+    {
+        if (AtilanInsanSayisi > 1 && AtilanInsanSayisi % 6 == 0)
+            Instantiate(Dev, RayPoint.position, transform.rotation);
+        else
+            Instantiate(Insan, RayPoint.position, transform.rotation);
+    }
+}
